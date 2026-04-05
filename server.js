b3e1cb5117fcc65442706520e5b10ec5334695c5
patapatallc-commits <patapatallc@gmail.com>
2026-04-05 17:05:38 -0400

    Add path and fs imports and fix uploads static serving

--- server.js
+++ server.js
@@ -94,9 +94,16 @@ const express = require("express");
 const axios = require("axios");
 require("dotenv").config();
 
+const path = require("path");
+const fs = require("fs");
 const app = express();
+const uploadsDir = path.join(__dirname, "uploads");
+
+if (!fs.existsSync(uploadsDir)) {
+  fs.mkdirSync(uploadsDir);
+}
 app.use(express.json({ limit: "20mb" }));
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/uploads", express.static(uploadsDir));
 const PORT = process.env.PORT || 10000;
 
 // =========================
